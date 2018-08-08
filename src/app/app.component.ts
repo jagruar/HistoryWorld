250dd3c4c69c467d404823d28744457267fd8d54
Jack Gruar <jack.gruar@outlook.com>
2018-08-08 12:08:01 +0100

    third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+  AngularFirestoreDocument
+} from "angularfire2/firestore";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+
+interface Post {
+  title: string;
+  content: string;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'my-new-project';
+  postsCol: AngularFirestoreCollection<Post>;
+  posts: Observable<Post[]>;
+
+  constructor(private afs: AngularFirestore) {}
+
+  ngOnInit() {
+    this.postsCol = this.afs.collection("posts");
+    this.posts = this.postsCol.valueChanges();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularFireModule } from "angularfire2";
+import { AngularFirestoreModule } from "angularfire2/firestore";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAHWNq1jCURgA4cJt62zWyZdXARx-qSdfI",
+  authDomain: "history-world.firebaseapp.com",
+  databaseURL: "https://history-world.firebaseio.com",
+  projectId: "history-world",
+  storageBucket: "history-world.appspot.com",
+  messagingSenderId: "853634216040"
+};
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, AboutComponent],
@@ -14,7 +25,9 @@ import { AboutComponent } from "./about/about.component";
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
